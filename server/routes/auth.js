1650a7e79ad69824961ffc685d1c69587fddb554
Nosferatu783 <smith.infosec@gmail.com>
2024-10-23 17:19:38 -0400

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const User = require('./server/models/user.js');
+const User = require('./../models/user.js');
 
 const router = express.Router();
 
